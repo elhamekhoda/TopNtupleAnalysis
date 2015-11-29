d385256c64aeee1d1dd769a60bc8bfd34289c1c6
dferreir <dferreir@cern.ch>
2015-11-29 14:29:20 +0000

    Updated plotting script to use 3.1 /fb of luminosity, in the latest GRL.

--- makeplots.sh
+++ makeplots.sh
@@ -1,6 +1,7 @@
 
 S=1
-LUMI=3.34
+#LUMI=3.34
+LUMI=3.1
 for hist in largeJetM largeJetEta largeJetPhi largeJetSd12 largeJet_tau32 largeJet_tau32_wta largeJet_tau21 largeJet_tau21_wta chi2 mwhad_res mthad_res mtlep_res jet0_m jet1_pt jet1_m jet2_pt jet2_m closejl_pt  closejl_minDeltaR MET_phi nBtagJets leadbJetPt lepEta lepPhi mwt  mtt nTrkBtagJets ; do
 for ch in boosted ; do
   for lep in e mu ; do
@@ -49,46 +50,46 @@ for ch in boosted ; do
     ../plotting/plotCompareNominal --mcOnly 1 --syst EG_SCALE_ALL__1up,EG_SCALE_ALL__1down --systTitles "e scale up,e scale down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_escale.pdf --smoothen $S
     ../plotting/plotCompareNominal --mcOnly 1 --syst MET_SoftTrk_ResoPara --systTitles "MET res. para." -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_metrespara.pdf --smoothen $S
     ../plotting/plotCompareNominal --mcOnly 1 --syst MET_SoftTrk_ResoPerp --systTitles "MET res. perp." -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_metresperp.pdf --smoothen $S
-    ../plotting/plotCompareNominal --mcOnly 1 --syst MET_SoftTrk_ScaleUp,MET_SoftTrk_ScaleDown --systTitles "MET scale up,MET scale down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_metscale.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btagbSF_0__1up,btagbSF_0__1down --systTitles "btag eff E0 up,btag eff E0 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagb0.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btagbSF_1__1up,btagbSF_1__1down --systTitles "btag eff E1 up,btag eff E1 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagb1.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btagbSF_2__1up,btagbSF_2__1down --systTitles "btag eff E2 up,btag eff E2 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagb2.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btagbSF_3__1up,btagbSF_3__1down --systTitles "btag eff E3 up,btag eff E3 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagb3.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btagbSF_4__1up,btagbSF_4__1down --systTitles "btag eff E4 up,btag eff E4 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagb4.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btagbSF_5__1up,btagbSF_5__1down --systTitles "btag eff E5 up,btag eff E5 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagb5.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btagcSF_0__1up,btagcSF_0__1down --systTitles "btag c mistag E0 up,btag c mistag E0 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagc0.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btagcSF_1__1up,btagcSF_1__1down --systTitles "btag c mistag E1 up,btag c mistag E1 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagc1.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btagcSF_2__1up,btagcSF_2__1down --systTitles "btag c mistag E2 up,btag c mistag E2 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagc2.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btagcSF_3__1up,btagcSF_3__1down --systTitles "btag c mistag E3 up,btag c mistag E3 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagc3.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_0__1up,btaglSF_0__1down --systTitles "btag l mistag E0 up,btag l mistag E0 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl0.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_1__1up,btaglSF_1__1down --systTitles "btag l mistag E1 up,btag l mistag E1 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl1.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_2__1up,btaglSF_2__1down --systTitles "btag l mistag E2 up,btag l mistag E2 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl2.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_3__1up,btaglSF_3__1down --systTitles "btag l mistag E3 up,btag l mistag E3 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl3.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_4__1up,btaglSF_4__1down --systTitles "btag l mistag E4 up,btag l mistag E4 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl4.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_5__1up,btaglSF_5__1down --systTitles "btag l mistag E5 up,btag l mistag E5 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl5.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_6__1up,btaglSF_6__1down --systTitles "btag l mistag E6 up,btag l mistag E6 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl6.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_7__1up,btaglSF_7__1down --systTitles "btag l mistag E7 up,btag l mistag E7 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl7.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_8__1up,btaglSF_8__1down --systTitles "btag l mistag E8 up,btag l mistag E8 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl8.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_9__1up,btaglSF_9__1down --systTitles "btag l mistag E9 up,btag l mistag E9 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl9.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_10__1up,btaglSF_10__1down --systTitles "btag l mistag E10 up,btag l mistag E10 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl10.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_11__1up,btaglSF_11__1down --systTitles "btag l mistag E11 up,btag l mistag E11 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl11.pdf --smoothen 0
+    ../plotting/plotCompareNominal --mcOnly 1 --syst MET_SoftTrk_ScaleUp,MET_SoftTrk_ScaleDown --systTitles "MET scale up,MET scale down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_metscale.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btagbSF_0__1up,btagbSF_0__1down --systTitles "btag eff E0 up,btag eff E0 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagb0.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btagbSF_1__1up,btagbSF_1__1down --systTitles "btag eff E1 up,btag eff E1 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagb1.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btagbSF_2__1up,btagbSF_2__1down --systTitles "btag eff E2 up,btag eff E2 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagb2.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btagbSF_3__1up,btagbSF_3__1down --systTitles "btag eff E3 up,btag eff E3 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagb3.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btagbSF_4__1up,btagbSF_4__1down --systTitles "btag eff E4 up,btag eff E4 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagb4.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btagbSF_5__1up,btagbSF_5__1down --systTitles "btag eff E5 up,btag eff E5 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagb5.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btagcSF_0__1up,btagcSF_0__1down --systTitles "btag c mistag E0 up,btag c mistag E0 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagc0.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btagcSF_1__1up,btagcSF_1__1down --systTitles "btag c mistag E1 up,btag c mistag E1 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagc1.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btagcSF_2__1up,btagcSF_2__1down --systTitles "btag c mistag E2 up,btag c mistag E2 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagc2.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btagcSF_3__1up,btagcSF_3__1down --systTitles "btag c mistag E3 up,btag c mistag E3 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagc3.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_0__1up,btaglSF_0__1down --systTitles "btag l mistag E0 up,btag l mistag E0 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl0.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_1__1up,btaglSF_1__1down --systTitles "btag l mistag E1 up,btag l mistag E1 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl1.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_2__1up,btaglSF_2__1down --systTitles "btag l mistag E2 up,btag l mistag E2 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl2.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_3__1up,btaglSF_3__1down --systTitles "btag l mistag E3 up,btag l mistag E3 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl3.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_4__1up,btaglSF_4__1down --systTitles "btag l mistag E4 up,btag l mistag E4 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl4.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_5__1up,btaglSF_5__1down --systTitles "btag l mistag E5 up,btag l mistag E5 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl5.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_6__1up,btaglSF_6__1down --systTitles "btag l mistag E6 up,btag l mistag E6 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl6.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_7__1up,btaglSF_7__1down --systTitles "btag l mistag E7 up,btag l mistag E7 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl7.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_8__1up,btaglSF_8__1down --systTitles "btag l mistag E8 up,btag l mistag E8 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl8.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_9__1up,btaglSF_9__1down --systTitles "btag l mistag E9 up,btag l mistag E9 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl9.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_10__1up,btaglSF_10__1down --systTitles "btag l mistag E10 up,btag l mistag E10 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl10.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btaglSF_11__1up,btaglSF_11__1down --systTitles "btag l mistag E11 up,btag l mistag E11 down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btagl11.pdf --smoothen 1
 
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btageSF_0__1up,btageSF_0__1down --systTitles "btag extrap up,btag extrap down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btage0.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst btageSF_1__1up,btageSF_1__1down --systTitles "btag extrap (c) up,btag extrap (c) down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btage1.pdf --smoothen 0
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btageSF_0__1up,btageSF_0__1down --systTitles "btag extrap up,btag extrap down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btage0.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst btageSF_1__1up,btageSF_1__1down --systTitles "btag extrap (c) up,btag extrap (c) down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_btage1.pdf --smoothen 1
 
-    ../plotting/plotCompareNominal --mcOnly 1 --syst eTrigSF__1up,eTrigSF__1down --systTitles "e trig. up,e trig. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_etrig.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst eRecoSF__1up,eRecoSF__1down --systTitles "e rec. up,e rec. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_erec.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst eIDSF__1up,eIDSF__1down --systTitles "e ID up,e ID down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_eid.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst eIsolSF__1up,eIsolSF__1down --systTitles "e isol. up,e isol. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_eisol.pdf --smoothen 0
+    ../plotting/plotCompareNominal --mcOnly 1 --syst eTrigSF__1up,eTrigSF__1down --systTitles "e trig. up,e trig. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_etrig.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst eRecoSF__1up,eRecoSF__1down --systTitles "e rec. up,e rec. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_erec.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst eIDSF__1up,eIDSF__1down --systTitles "e ID up,e ID down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_eid.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst eIsolSF__1up,eIsolSF__1down --systTitles "e isol. up,e isol. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_eisol.pdf --smoothen 1
 
-    ../plotting/plotCompareNominal --mcOnly 1 --syst muTrigStatSF__1up,muTrigStatSF__1down --systTitles "mu trig. stat. up,mu trig. stat. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_mutrigstat.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst muTrigSystSF__1up,muTrigSystSF__1down --systTitles "mu trig. syst. up,mu trig. syst. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_mutrigsyst.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst muIDStatSF__1up,muIDStatSF__1down --systTitles "mu ID stat. up,mu ID stat. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_muidstat.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst muIDSystSF__1up,muIDSystSF__1down --systTitles "mu ID syst. up,mu ID syst. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_muidsyst.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst muIsolStatSF__1up,muIsolStatSF__1down --systTitles "mu isol. stat. up,mu isol. stat. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_muisolstat.pdf --smoothen 0
-    ../plotting/plotCompareNominal --mcOnly 1 --syst muIsolSystSF__1up,muIsolSystSF__1down --systTitles "mu isol syst. up,mu isol syst. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_muisolsyst.pdf --smoothen 0
+    ../plotting/plotCompareNominal --mcOnly 1 --syst muTrigStatSF__1up,muTrigStatSF__1down --systTitles "mu trig. stat. up,mu trig. stat. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_mutrigstat.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst muTrigSystSF__1up,muTrigSystSF__1down --systTitles "mu trig. syst. up,mu trig. syst. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_mutrigsyst.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst muIDStatSF__1up,muIDStatSF__1down --systTitles "mu ID stat. up,mu ID stat. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_muidstat.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst muIDSystSF__1up,muIDSystSF__1down --systTitles "mu ID syst. up,mu ID syst. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_muidsyst.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst muIsolStatSF__1up,muIsolStatSF__1down --systTitles "mu isol. stat. up,mu isol. stat. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_muisolstat.pdf --smoothen 1
+    ../plotting/plotCompareNominal --mcOnly 1 --syst muIsolSystSF__1up,muIsolSystSF__1down --systTitles "mu isol syst. up,mu isol syst. down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_muisolsyst.pdf --smoothen 1
 
-    ../plotting/plotCompareNominal --mcOnly 1 --syst boostedWSF__1up,boostedWSF__1down --systTitles "boosted W C/A SF up,boosted W C/A SF down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_wmodel.pdf --smoothen 0
+    ../plotting/plotCompareNominal --mcOnly 1 --syst boostedWSF__1up,boostedWSF__1down --systTitles "boosted W C/A SF up,boosted W C/A SF down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_${lep}_wmodel.pdf --smoothen 1
 
     ../plotting/plotCompareNominal --mcOnly 1 --syst btageSF_0__1up,btageSF_0__1down --systTitles "btag extrap up,btag extrap down" -l $LUMI -c $lep -p $ch -h mtt -o syst_${ch}_mtt_signal_${lep}_btage0.pdf -C ../plotting/config_sig.txt --smoothen 0
 
