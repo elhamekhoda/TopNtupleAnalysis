6791b3cf98699129bde24433f0d33e02227a02c6
dferreir <dferreir@cern.ch>
2016-03-14 15:30:00 +0000

    Added macro to inject negative signal or negative step function for debugging.

--- limits/binned/injectStep.py
+++ limits/binned/injectStep.py
@@ -4,13 +4,15 @@ from ROOT import *
 from os import system
 system("cp -f hist_bkg.root hist_data_inj.root")
 
-mu = -2.0
+stepSize = -0.25 # 25% discrepancy
+firstBin = 2e3
+lastBin = 3e3
 
 f = TFile("hist_data_inj.root", "update")
-fs = TFile("hist_Zprime3000.root")
 for i in ["xmttboostede", "xmttboostedmu", "xlargeJetMboostede", "xlargeJetMboostedmu", "xlargeJetPtboostede", "xlargeJetPtboostedmu"]:
     h = f.Get(i)
-    hs = fs.Get(i)
-    h.Add(hs, mu)
+    for b in range(1, h.GetNbinsX()):
+        if h.GetXaxis().GetBinCenter(b) > firstBin and h.GetXaxis().GetBinCenter(b) < lastBin:
+            h.SetBinContent(b, h.GetBinContent(b)*(1 + stepSize))
     f.cd()
     h.Write(i, TObject.kOverwrite)
