af3123bf31f4caba97a3874c0d08b6f559b28174
dferreir <dferreir@cern.ch>
2016-03-14 18:59:12 +0000

    Moved step to higher values to simulate muon channel effect in data.

--- limits/binned/injectStep.py
+++ limits/binned/injectStep.py
@@ -4,20 +4,26 @@ from ROOT import *
 from os import system
 #system("cp -f hist_bkg.root hist_data_inj.root")
 
-stepSize = -0.25 # 25% discrepancy
-firstBin = 2e3
-lastBin = 3e3
+stepSizeE = -0.10 # 15% discrepancy
+stepSizeMu = -0.20 # 25% discrepancy
+firstBin = 2.3e3
+lastBin = 3.2e3
 
 fb = TFile("hist_bkg.root")
 f = TFile("hist_data_inj.root", "recreate")
 for i in ["xmttboostede", "xmttboostedmu", "xlargeJetMboostede", "xlargeJetMboostedmu", "xlargeJetPtboostede", "xlargeJetPtboostedmu"]:
+    if 'boostede' in i:
+        stepSize = stepSizeE
+    elif 'boostedmu' in i:
+        stepSize = stepSizeMu
+
     hb = fb.Get(i)
     f.cd()
     h = hb.Clone(i+"clone")
     for b in range(1, h.GetNbinsX()):
         if h.GetXaxis().GetBinCenter(b) > firstBin and h.GetXaxis().GetBinCenter(b) < lastBin:
             if h.GetBinContent(b)*(1 + stepSize) > 0:
-                h.SetBinContent(b, floor(h.GetBinContent(b)*(1 + stepSize)))
+                h.SetBinContent(b, round(h.GetBinContent(b)*(1 + stepSize), -1))
             else:
                 h.SetBinContent(b, 0)
         else:
