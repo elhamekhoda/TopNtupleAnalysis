93fd0f87d0ec9126a8d53068f41a6bf5221dec56
dferreir <dferreir@cern.ch>
2016-05-12 16:25:00 +0000

    Added simple Python code for simpler tests.

---
+++ pyHistograms/helpers.py
@@ -0,0 +1,198 @@
+
+from analysis import idClasses
+import ROOT
+from array import array
+
+def initBinds():
+    bind_neutrino = '''
+#include "TopNtupleAnalysis/TtresNeutrinoBuilder.h"
+#include "TopNtupleAnalysis/TtresChi2.h"
+
+#include "Root/TtresNeutrinoBuilder.cxx"
+#include "Root/TtresChi2.cxx"
+
+#include "TopNtupleAnalysis/WeakCorrScaleFactorParam.h"
+#include "Root/WeakCorrScaleFactorParam.cxx"
+
+#include "TLorentzVector.h"
+#include <vector>
+
+class WrapperExtras {
+  public:
+    TtresNeutrinoBuilder m_neutrinoBuilder;
+    TtresChi2 m_chi2;
+    WeakCorr::WeakCorrScaleFactorParam m_ewkTool;
+    WrapperExtras() : m_neutrinoBuilder("MeV"), m_chi2("MeV"), m_ewkTool("../share/EWcorr_param.root") {
+      m_chi2.Init(TtresChi2::DATA2015_MC15);
+    }
+    TLorentzVector getNu(TLorentzVector l, double met, double met_phi) {
+      std::vector<TLorentzVector *> vec_nu = m_neutrinoBuilder.candidatesFromWMass_Rotation(&l, met, met_phi, true);
+      TLorentzVector nu;
+      if (vec_nu.size() > 0) {
+        nu = *(vec_nu[0]);
+	for (size_t z = 0; z < vec_nu.size(); ++z) delete vec_nu[z];
+	vec_nu.clear();
+      }
+      return nu;
+    }
+
+    std::map<std::string, double> getMtt(TLorentzVector lep, std::vector<TLorentzVector> jets, std::vector<bool> btag, TLorentzVector met) {
+      // inputs 
+      // LEPTON --> TLorentzVector for your lepton
+      // vjets -->  std::vector<TLorentzVector*> for the jets
+      // vjets_btagged --> std::vector<bool> to say if the jets are btagged or not
+      // met --> TLorentzVector for your MET
+
+      // outputs, they will be filled by the TTBarLeptonJetsBuilder_chi2
+      int  igj3, igj4; // index for the Whad
+      int igb3, igb4; // index for the b's
+      int  ign1;  // index for the neutrino (because chi2 can test both pz solution)
+      double chi2ming1, chi2ming1H, chi2ming1L;
+
+      std::vector<TLorentzVector *> vjets;
+      std::vector<bool> vjets_btagged;
+      for (size_t z = 0; z < jets.size(); ++z) {
+        vjets.push_back(new TLorentzVector(0,0,0,0));
+        vjets[z]->SetPtEtaPhiE(jets[z].Perp(), jets[z].Eta(), jets[z].Phi(), jets[z].E());
+        // https://twiki.cern.ch/twiki/bin/viewauth/AtlasProtected/BTagingxAODEDM
+        // https://twiki.cern.ch/twiki/bin/view/AtlasProtected/BTaggingBenchmarks
+        vjets_btagged.push_back(btag[z]);
+      }
+      bool status = m_chi2.findMinChiSquare(&lep, &vjets, &vjets_btagged, &met, igj3, igj4, igb3, igb4, ign1, chi2ming1, chi2ming1H, chi2ming1L); 
+    
+      float chi2Value = 1000000; // log10(1000000) = 6
+      float mwh = -1;
+      float mtl = -1;
+      float mth = -1;
+      float mtt = -1;
+
+      if (status) {
+        chi2Value = chi2ming1;
+	mwh = m_chi2.getResult_Mwh();
+	mtl = m_chi2.getResult_Mtl();
+	mth = m_chi2.getResult_Mth();
+	mtt = m_chi2.getResult_Mtt();
+      }
+    
+      for (size_t z = 0; z < vjets.size(); ++z) {
+        delete vjets[z];
+      }
+      vjets.clear();
+      vjets_btagged.clear();
+      std::map<std::string, double> ret;
+      ret["mtt"] = mtt;
+      ret["mtl"] = mtl;
+      ret["mth"] = mth;
+      ret["mwh"] = mwh;
+      return ret;
+    }
+    double getEWK(TLorentzVector top, TLorentzVector topbar, int initial_type) {
+      WeakCorr::ScaleFactor sf; 
+      float t_pt = top.Perp();
+      float t_eta = top.Eta();
+      float t_phi = top.Phi();
+      float t_m = top.M();
+
+      float tbar_pt = topbar.Perp();
+      float tbar_eta = topbar.Eta();
+      float tbar_phi = topbar.Phi();
+      float tbar_m = topbar.M();
+      sf = m_ewkTool.getScaleFactor(t_pt, t_eta, t_phi, t_m, tbar_pt, tbar_eta, tbar_phi, tbar_m, initial_type);
+      return sf.nominal;
+    }
+};
+'''
+    # hack to get the proper paths
+    import os.path
+    import os
+    if not (os.path.isfile('TopNtupleAnalysis') or os.path.islink('TopNtupleAnalysis')):
+        os.system('ln -s ../TopNtupleAnalysis .')
+    if not (os.path.isfile('Root') or os.path.islink('Root')):
+        os.system('ln -s ../Root .')
+    ROOT.gInterpreter.ProcessLine(bind_neutrino)
+    return ROOT.WrapperExtras()
+
+wrapperC = initBinds()
+
+class HistogramService:
+    fi = None
+    histSuffixes = [] # systematic copies of histograms
+    h = {} # map of histogram names to map of systematics to histograms
+
+    # outputFiles is a map of a channel to an output file name
+    # suffixes is a list of histogram suffixes to make
+    def __init__(self, suffixes, outputFile):
+	self.fi = ROOT.TFile(outputFile, "recreate")
+	self.histSuffixes = suffixes
+        pass
+    def add(self, hName, nBins, xLow, xHigh):
+        self.h[hName] = {}
+	for s in self.histSuffixes:
+	    self.h[hName][s] = ROOT.TH1D(hName+s, "", nBins, xLow, xHigh)
+    def addVar(self, hName, nBinsList):
+        ar = array("d", nBinsList)
+        self.fi.cd()
+        self.h[hName] = {}
+	for s in self.histSuffixes:
+	    self.h[hName][s] = ROOT.TH1D(hName+s, "", len(nBinsList) - 1, ar)
+
+    def write(self):
+        self.fi.cd()
+	for hName in self.h:
+	    for s in self.histSuffixes:
+	        self.h[hName][s].Write()
+
+
+class AnalysisDirector:
+    def __init__(self):
+        self.allClasses = []
+
+    def construct(self, builderName, hSvc, channel):
+        targetClass = getattr(idClasses, builderName)
+        instance = targetClass(hSvc, channel)
+        self.allClasses.append(instance)
+	return instance
+
+def loadXsec(m, fName):
+    f = open(fName)
+    for l in f.readlines():
+        if l[0] == '#':
+	    continue
+        lsplit = l.split()
+	if len(lsplit) == 0:
+	    continue
+	if len(lsplit) < 3:
+	    print "I cannot understand this line:", lsplit
+	    continue
+	m[int(lsplit[0])] = float(lsplit[1])*float(lsplit[2])
+
+def addFilesInChain(c, txtFileOption):
+    for f in txtFileOption.split(','):
+        txtf = open(f)
+        for l in txtf.readlines():
+	    if l[-1] == '\n':
+	        l = l[0:-1]
+            c.Add(l)
+
+class Event:
+    def __init__(self):
+        pass
+
+MV2C20_CUT = -0.3098
+# TODO
+def readEvent(mt):
+    return mt
+    #sel = Event()
+    #sel.weight_mc = mt.weight_mc
+    #sel.weight_pileup = mt.weight_pileup
+
+# TODO
+listEWK = [410000, 301528, 301529, 301530, 301531, 301532]
+def applyEWK(sel):
+    top = ROOT.TLorentzVector()
+    top.SetPtEtaPhiM(sel.MC_t_pt, sel.MC_t_eta, sel.MC_t_phi, sel.MC_t_m)
+    topbar = ROOT.TLorentzVector()
+    topbar.SetPtEtaPhiM(sel.MC_tbar_pt, sel.MC_tbar_eta, sel.MC_tbar_phi, sel.MC_tbar_m)
+    w = wrapperC.getEWK(top, topbar, sel.initial_type)
+    return w
+
---
+++ pyHistograms/makeHistograms.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+from helpers import *
+import math
+from ROOT import *
+from analysis import *
+from optparse import OptionParser
+
+def main():
+
+
+    parser = OptionParser()
+    parser.add_option("-d", "--data",
+                      action="store_true", dest="data", default=False,
+                      help="Is this data?", metavar="BOOL")
+    parser.add_option("-f", "--files",
+                      dest="files", default="input.txt",
+    		  help="Text file with list of input files.", metavar="FILE")
+    parser.add_option("-F", "--fullFiles",
+                      dest="fullFiles", default="",
+    		  help="Text file with list of all input files for normalisation calculation.", metavar="FILE")
+    parser.add_option("-A", "--analysis",
+                      dest="analysis", default="AnaTtresSL",
+    		  help="Analysis code to run.", metavar="ANALYSIS")
+    parser.add_option("-o", "--output",
+                      dest="output", default="re:hist_re.root,rmu:hist_rmu.root,be:hist_be.root,bmu:hist_bmu.root",
+    		  help="Comma-separated list of output ROOT files.", metavar="FILES")
+    parser.add_option("-s", "--systs",
+                      dest="systs", default="nominal",
+    		  help="Comma-separated list of systematic uncertainties in TTrees in the input file.", metavar="SYSTEMATICS")
+    
+    (options, args) = parser.parse_args()
+    
+    if options.fullFiles == '':
+        options.fullFiles = options.files
+    
+    Xsec = {}
+    
+    sumOfWeights = {} # map of DSID to sum of weights
+    # TODO
+    pdfSumOfWeights = {} # map of DSID to map of PDF variation names to sum of weights
+    
+    if not options.data:
+        t_sumWeights = TChain("sumWeights")
+        addFilesInChain(t_sumWeights, options.fullFiles)
+        for k in range(0, t_sumWeights.GetEntries()):
+            t_sumWeights.GetEntry(k)
+    	if not t_sumWeights.dsid in sumOfWeights:
+    	    sumOfWeights[t_sumWeights.dsid] = 0
+    	sumOfWeights[t_sumWeights.dsid] += t_sumWeights.totalEventsWeighted
+    
+    loadXsec(Xsec, "../scripts/XSection-MC15-13TeV-ttres.data")
+    loadXsec(Xsec, "../../TopDataPreparation/data/XSection-MC15-13TeV.data")
+    
+    # systematics list
+    systList = options.systs.split(',')
+    
+    # load analysis code
+    histSuffixes = []
+    for item in systList:
+        if item == 'nominal':
+	    histSuffixes.append('')
+	else:
+	    histSuffixes.append(item)
+    hSvc = {}
+    channels = {}
+    for k in options.output.split(','):
+        channels[k.split(':')[0]] = k.split(':')[1]
+    for k in channels:
+        hSvc[k] = HistogramService(histSuffixes, channels[k])
+    director = AnalysisDirector()
+    analysisCode = {}
+    for k in channels:
+        analysisCode[k] = director.construct(options.analysis, hSvc[k], k)
+    
+    for s in systList:
+        # s is nominal, or the name of systematic
+        treeName = s # systematic name is the same as the TTree name
+        mt = TChain(treeName)
+	suffix = s
+	if suffix == 'nominal':
+	    suffix = ''
+        addFilesInChain(mt, options.files)
+        for k in range(0, mt.GetEntries()):
+	    mt.GetEntry(k)
+            if k % 1000 == 0:
+    	        print "(",treeName,") Entry ", k, "/", mt.GetEntries()
+    	    sel = readEvent(mt)
+    	    channel = sel.mcChannelNumber
+    	    weight = 1
+    	    if not options.data:
+    	        weight *= sel.weight_mc
+    	        weight *= Xsec[channel]
+		# put the remainder in a function to calculate the reweighting
+    	        weight *= sel.weight_pileup
+    	        if channel in listEWK:
+    	            weight *= applyEWK(sel)
+                    weight *= sel.weight_leptonSF
+    	        btagsf = 1.0
+    	        for bidx in range(0, len(sel.tjet_mv2c20)):
+    	            pb = TLorentzVector(sel.tjet_pt[bidx], sel.tjet_eta[bidx], sel.tjet_phi[bidx], sel.tjet_e[bidx])
+    	            if pb.Perp() > 10e3 and math.fabs(pb.Eta()) < 2.5 and sel.tjet_numConstituents[bidx] >= 2:
+    	                btagsf *= sel.tjet_bTagSF_70[bidx]
+    	        weight *= btagsf
+    	        weight /= sumOfWeights[channel]
+    	    nBtags = 0
+    	    for bidx in range(0, len(sel.tjet_mv2c20)):
+    	        pb = TLorentzVector(sel.tjet_pt[bidx], sel.tjet_eta[bidx], sel.tjet_phi[bidx], sel.tjet_e[bidx])
+    	        if pb.Perp() > 10e3 and math.fabs(pb.Eta()) < 2.5 and sel.tjet_numConstituents[bidx] >= 2:
+                    if sel.tjet_mv2c20 > MV2C20_CUT:
+    	                nBtags += 1
+    
+    	    if nBtags < 1:
+    	        continue
+            for ana in analysisCode:
+    	        analysisCode[ana].run(sel, suffix, weight)
+    
+    for k in hSvc:
+        hSvc[k].write()
+
+if __name__ == "__main__":
+    main()
+
+    
