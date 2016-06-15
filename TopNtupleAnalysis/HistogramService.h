f4d265af04eca6caaf6c83d19d1fb90640002f32
sanmay <sanmay@cern.ch>
2016-06-15 12:34:12 +0000

    TProfile added in HistogramService class to study Delta-R profile plot

--- TopNtupleAnalysis/HistogramService.h
+++ TopNtupleAnalysis/HistogramService.h
@@ -10,6 +10,7 @@
 #include "TH2D.h"
 #include "TH1F.h"
 #include "TH2F.h"
+#include "TProfile.h"
 #include <vector>
 #include <map>
 #include "TFile.h"
@@ -29,10 +30,14 @@ class HistogramService {
     void create2D(const std::string &name, const std::string &title, int binsX, double lowX, double highX, int binsY, double lowY, double highY, bool allSyst = true);
     void create2DVar(const std::string &name, const std::string &title, int binsX, double *vecX, int binsY, double *vecY, bool allSyst = true);
 
+//    void create1DVarP(const std::string &name, const std::string &title, int binsX, double *bins, double lowY, double highY, bool allSyst = true);
+    void create1DP(const std::string &name, const std::string &title, int binsX, double lowX, double highX, double lowY, double highY, bool allSyst = true);
+
     //TH1D *&h1D(const std::string &name, const std::string &trigger, const std::string &systematics);
     //TH2D *&h2D(const std::string &name, const std::string &trigger, const std::string &systematics);
     TH1F *&h1D(const std::string &name, const std::string &trigger, const std::string &systematics);
     TH2F *&h2D(const std::string &name, const std::string &trigger, const std::string &systematics);
+    TProfile *&p1D(const std::string &name, const std::string &trigger, const std::string &systematics);
 
     std::string concat(const std::string &name, const std::string &suffix);
 
@@ -46,6 +51,7 @@ class HistogramService {
 
     TH1F *m_dummy1D;
     TH2F *m_dummy2D; // useless histograms which will be filled whenever histograms have no systematics to be filled
+    TProfile *m_dummy1DP;
     std::vector<std::string> *m_dummyVS;
 
     std::string m_filename;
@@ -63,11 +69,13 @@ class HistogramService {
     //std::map<std::string, std::map<std::string, std::map<std::string, TH2D *> > > h_hist2D;
     std::map<std::string, std::map<std::string, std::map<std::string, TH1F *> > > h_hist1D;
     std::map<std::string, std::map<std::string, std::map<std::string, TH2F *> > > h_hist2D;
+    std::map<std::string, std::map<std::string, std::map<std::string, TProfile *> > > h_profile1D;
     std::map<std::string, std::vector<std::string> *> m_vs;
 
     // lists of histograms with no syst. variations
     std::vector<std::string> m_noSystHist1D;
     std::vector<std::string> m_noSystHist2D;
+    std::vector<std::string> m_noSystProfile1D;
 
   public:
 
