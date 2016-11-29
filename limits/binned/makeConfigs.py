b8871ed55ea1a2c74edb2aac834dbfb2b1244d25
scalvet <scalvet@cern.ch>
2016-11-29 10:23:00 +0000

    add script for limit at LPC

--- limits/binned/makeConfigs.py
+++ limits/binned/makeConfigs.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
-from os import system,getcwd
+from os import system,getcwd,path
 from time import time
 from inputs import *
 import socket, random, re
 
+site="DESY"
+if socket.gethostname().find("clratl") != -1:
+	site="LPC"
+print "@"+site+"! "
+
 # change those:
-setdir = '/afs/desy.de/user/d/danilo/xxl/af-atlas/Top2418'
-mydir = '/nfs/dust/atlas/user/danilo/hists_sr2418_jes/TRexFitter_trs'
+if site=="DESY":
+   setdir = '/afs/desy.de/user/d/danilo/xxl/af-atlas/Top2418'
+   mydir = '/nfs/dust/atlas/user/danilo/hists_sr2418_jes/TRexFitter_trs'
+elif site=="LPC":
+   mydir = '/AtlasDisk/users/scalvet/Zprime/LimitSetting/TopNtupleAnalysis/limits/binned/Jamboree/test1/TRExFitter'
+   version="firstTest"
+   version="secondTest"
+
+
 
 def fixFile(template, final, i, doBOnlyFit):
   fr = open(template, 'r')
@@ -51,6 +63,74 @@ Systematic: "eftScale"
 
 
 def jobSubmit(suf):
+	if site=="DESY":
+		jobSubmitDESY(suf)
+	elif site=="LPC":
+		jobSubmitLPC(suf)
+
+
+def jobSubmitLPC(stuf):
+        print stuf
+	
+        here = getcwd()
+        configName = 'ttres_'+stuf+'.config'
+	
+	system('mkdir -p '+version)
+	if path.exists(version+"/t.tar.gz")==False:
+		print "Making tarball..."
+		com = "tar -czf "+version+"/t.tar.gz --exclude="+version+"/*root --exclude=inputFiles/ --exclude=JobTtres *"
+		system(com)
+	
+	system('cp '+configName+' '+version)
+	
+        scriptName=version+"/script_"+stuf+".sh"
+        f = open(scriptName,'w')       
+        f.write('#!/bin/bash\n')
+        f.write('echo running on $HOSTNAME\n')
+        f.write('\n')
+	
+        workDir = re.sub('\.','',str(time())+`random.randint(0, 1000000)`)
+        WORKDIR = "/users_local1/$LOGNAME/"+workDir
+               
+        f.write('\n' + 'mkdir -p '+WORKDIR+'\n')
+        f.write('\n' + 'cd '+WORKDIR+'\n')
+        f.write('export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase\n')
+        f.write('source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh\n')
+        f.write('localSetupROOT\n')
+        f.write('\n')
+        f.write('cp -f '+here+"/"+version+'/t.tar.gz .\n')
+        f.write('tar -xzvf t.tar.gz\n')
+        f.write('echo "untaring done!" \n\n')
+        f.write('cp -f '+here+"/"+version+'/'+configName+' .\n')
+        f.write('./myFit.exe h '+configName+' > log_'+stuf+'_h\n')
+        f.write('./myFit.exe d '+configName+' > log_'+stuf+'_d\n')
+        f.write('./myFit.exe w '+configName+' > log_'+stuf+'_w\n')
+        f.write('./myFit.exe f '+configName+' > log_'+stuf+'_f\n')
+        f.write('./myFit.exe p '+configName+' > log_'+stuf+'_p\n')
+        if configName.find("bkg")!=-1:
+                #do nothing
+                a=1
+        else:
+                f.write('./myFit.exe l '+configName+' > log_'+stuf+'_l\n')
+                f.write('./myFit.exe s '+configName+' > log_'+stuf+'_s\n')
+                #f.write('./myFit.exe r '+configName+' > log_'+stuf+'_r\n')
+       
+       
+       
+        outDir = './JobTtres_'+stuf
+       
+        f.write('cp -rf '+outDir+' '+here+'/'+version+'\n')
+        f.write('cp -f log_'+stuf+'_*  '+here+'/'+version+'/'+outDir+'/\n')
+        f.write('rm -rf '+WORKDIR+'\n')
+        f.close()
+       
+        com= 'qsub -N '+stuf+' -q multi64sl6@clratlserv05  -e '+here+'/'+version+' -o '+here+'/'+version+' '+scriptName
+        #com= 'qsub -N '+stuf+' -q fast64sl6@clratlserv03  -e '+here+'/'+version+' -o '+here+'/'+version+' '+scriptName
+               
+        print com
+
+
+def jobSubmitDESY(suf):
 	runfile = 'submit_%s.sh' % suf
 	logfile = mydir+'/stdout_%s.txt'%suf
 	queue = 'long.q'
