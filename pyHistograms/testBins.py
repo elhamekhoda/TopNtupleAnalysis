b3ed4d5b8a8956a163b514709092d1ee50af6639
dferreir <dferreir@cern.ch>
2016-12-02 14:04:43 +0000

    Fixed script that chooses mtt binning.

--- pyHistograms/testBins.py
+++ pyHistograms/testBins.py
@@ -3,33 +3,35 @@
 import math
 import ROOT
 from optparse import OptionParser
+from array import array
 
-def getBinning(h):
+def getBinning(h, threshold):
   pos = 1
-  binning = []
-  binEdges = []
-  while pos < h.GetNbinsX():
+  binning = [1]
+  binEdges = [h.GetXaxis().GetBinLowEdge(1)]
+  while pos < h.GetNbinsX()+1:
     currentBin = h.GetXaxis().GetBinLowEdge(pos)
-    if pos == 1:
-      binning.append(pos)
-      binEdges.append(currentBin)
-      pos += 1
-      continue
     contentFromLastBin = 0
     errorFromLastBin = 0
-    for k in range(binning[-1]+1, pos+1):
+    binAfterLast = 1
+    if len(binning) > 1:
+      binAfterLast = binning[-1]+1
+    for k in range(binAfterLast, pos):
       contentFromLastBin += h.GetBinContent(k)
       errorFromLastBin += h.GetBinError(k)**2
     errorFromLastBin = errorFromLastBin**0.5
     if contentFromLastBin == 0:
       pos += 1
       continue
-    elif errorFromLastBin/contentFromLastBin < threshold:
+    if errorFromLastBin/contentFromLastBin < threshold:
       binning.append(pos)
       binEdges.append(currentBin)
-      pos += 1
     # else, we need to add more bins
     pos += 1
+  if binning[-1] != pos:
+    binning.append(pos)
+    binEdges.append(h.GetXaxis().GetBinLowEdge(pos))
+
 
   return [binning, binEdges]
 
@@ -37,21 +39,21 @@ def fillStat(s, rs, h):
   for i in range(1, h.GetNbinsX()):
     s.SetPoint(i-1, h.GetBinCenter(i), h.GetBinError(i))
     if h.GetBinContent(i) > 0:
-      rs.SetPoint(i-1, h.GetBinCenter(i), h.GetBinError(i)/h.GetBinContent(i))
+      rs.SetPoint(i-1, h.GetXaxis().GetBinLowEdge(i), h.GetBinError(i)/h.GetBinContent(i))
     else:
-      rs.SetPoint(i-1, h.GetBinCenter(i), 0)
+      rs.SetPoint(i-1, h.GetXaxis().GetBinLowEdge(i), 0)
 
 
 def main():
 
   parser = OptionParser()
   parser.add_option("-f", "--file",
-                    dest="file", default="re_ttbar.root",
+                    dest="file", default="re_ttall.root",
                     help="Input file.", metavar="FILE")
   parser.add_option("-t", "--threshold",
                     dest="threshold", default="0.05",
                     help="Maximum allowed statistical error.", metavar="STATERROR")
-  parser.add_option("-h", "--histogram",
+  parser.add_option("-H", "--histogram",
                     dest="histogram", default="mtt",
                     help="Histogram to choose binning", metavar="HISTOGRAM")
   (options, args) = parser.parse_args()
@@ -62,22 +64,22 @@ def main():
   f = ROOT.TFile(infile)
   h = f.Get(options.histogram)
 
-  [binning, binEdges] = getBinning(h)
+  [binning, binEdges] = getBinning(h, threshold)
   print "binning found: ", binning, binEdges
 
   hrebin = h.Rebin(len(binEdges)-1, h.GetName()+"rebin", array('d', binEdges))
 
-  gstat = ROOT.TGraph(h.GetNbinsX())
-  grelstat = ROOT.TGraph(h.GetNbinsX())
+  gstat = ROOT.TGraph(h.GetNbinsX()-1)
+  grelstat = ROOT.TGraph(h.GetNbinsX()-1)
 
-  grstat = ROOT.TGraph(hrebin.GetNbinsX())
-  grrelstat = ROOT.TGraph(hrebin.GetNbinsX())
+  grstat = ROOT.TGraph(hrebin.GetNbinsX()-1)
+  grrelstat = ROOT.TGraph(hrebin.GetNbinsX()-1)
 
   fillStat(gstat, grelstat, h)
-  fillStat(grstat, grrelstat, h)
+  fillStat(grstat, grrelstat, hrebin)
 
   glist = [gstat, grelstat, grstat, grrelstat]
-  tlist = ["Statistical unc.", "Relative stat. unc. [\%]", "Statistical unc.", "Relative stat. unc. [\%]"]
+  tlist = ["Statistical unc.", "Relative stat. unc. [%]", "Statistical unc.", "Relative stat. unc. [%]"]
   nlist = ["stat.eps", "statrel.eps", "stat_rebin.eps", "statrel_rebin.eps"]
 
   for g in range(0, len(glist)):
@@ -90,7 +92,7 @@ def main():
       glist[g].SetMaximum(1.0)
     glist[g].Draw("ac*")
     if "statrel" in nlist[g]:
-      l = ROOT.TLine(h.GetXaxis().GetBinLowEdge(1), threshold, h.GetXaxis().GetBinUpEdge(h.GetNbinsX()), threshold)
+      l = ROOT.TLine(glist[g].GetX()[0], threshold, glist[g].GetX()[glist[g].GetN()-1], threshold)
       l.SetLineColor(ROOT.kBlack)
       l.SetLineWidth(2)
       l.SetLineStyle(ROOT.kDashed)
