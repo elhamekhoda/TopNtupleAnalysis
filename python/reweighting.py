e85d65e0b705ad270237da92a5466485211aaa69
Yu-Heng Chen <yu-heng.chen@cern.ch>
2018-12-05 23:02:50 +0100

    Change: now events in the inclusive ttbar sample (nonallhad:410470, allhad:410471) with truth mtt > 1.1 TeV are correctly vetoed. To avoid this, use the  option

--- python/reweighting.py
+++ python/reweighting.py
@@ -1,4 +1,6 @@
+import helpers
 import ROOT
+logger = helpers.getLogger('TopNtupleAnalysis.reweighting')
 
 class Reweighter(object):
     SYSTS = {'*': 0}
@@ -27,6 +29,8 @@ class Reweighter(object):
 
 class EWKCorrection(Reweighter):
     RUN_NUMBERS = [410471, 410470, 410000, 301528, 301529, 301530, 301531, 301532]
+    RUN_NUMBERS += [410633, 410634, 410635, 410636, 410637] # mttsliced nonallhad
+    RUN_NUMBERS += [410284, 410285, 410286, 410287, 410288] # mttsliced allhad
     SYSTS = {'ttEWK__1up': 1, 'ttEWK__1down': 2, '*': 0}
     @classmethod
     def get_SF(cls, ev, s):
@@ -40,3 +44,42 @@ class EWKCorrection(Reweighter):
         topbar.SetPtEtaPhiM(ev.MC_tbar_pt, ev.MC_tbar_eta, ev.MC_tbar_phi, ev.MC_tbar_m)
         w = ROOT.TopNtupleAnalysis.getEWK(top, topbar, ev.initial_type, s)
         return w
+
+class NNLOReweighting(Reweighter):
+    RUN_NUMBERS = [410471, 410470, 410000, 301528, 301529, 301530, 301531, 301532]
+    RUN_NUMBERS += [410633, 410634, 410635, 410636, 410637] # mttsliced nonallhad
+    RUN_NUMBERS += [410284, 410285, 410286, 410287, 410288] # mttsliced allhad
+    SYSTS = {'ttNNLO_seqExtended__1up': 1, 'ttNNLO_seqExtended__1down': -1,
+             'ttNNLO_topPt__1up': 2, 'ttNNLO_topPt__1down': -2,
+             'ttNNLO_topPtDiff__1up': 3, 'ttNNLO_topPtDiff__1down': -3,
+             'ttNNLO_seqExtended__1up': 4, 'ttNNLO_seqExtended__1down': -4,
+             '*': 0
+             }
+    @classmethod
+    def initNNLO(cls, mcChannelNumber):
+        if mcChannelNumber not in cls.RUN_NUMBERS:
+            logger.info('<{}> is not a registered ttbar sample. NNLO Reweighter will not be activated.'.format(mcChannelNumber))
+        else:
+            logger.info('<{}> is a registered ttbar sample. NNLO Reweighter will be activated.'.format(mcChannelNumber))
+            if mcChannelNumber in [301528, 301529, 301530, 301531, 301532]:
+                mcChannelNumber = 410000
+            elif mcChannelNumber in [410284, 410285, 410286, 410287, 410288]:
+                mcChannelNumber = 410471
+            elif mcChannelNumber in [410633, 410634, 410635, 410636, 410637]:
+                mcChannelNumber = 410470
+            ROOT.TopNtupleAnalysis.InitNNLO(mcChannelNumber)
+    @classmethod
+    def get_SF(cls, ev, s):
+        if ev.mcChannelNumber == 0:
+            return 1.
+        abss = abs(s) # we only have 1up always
+        if abss == 0:
+            return 1.
+        if abss == 1:
+            return ROOT.TopNtupleAnalysis.getNNLOWeight(ev.MC_ttbar_afterFSR_pt, ev.MC_t_afterFSR_pt, 1)
+        if abss == 2:
+            return ROOT.TopNtupleAnalysis.getNNLOWeight(ev.MC_ttbar_afterFSR_pt, ev.MC_t_afterFSR_pt, 0)
+        if abss == 3:
+            return ROOT.TopNtupleAnalysis.getNNLOWeight(ev.MC_ttbar_afterFSR_pt, ev.MC_t_afterFSR_pt, 2)
+        if abss == 4:
+            return ROOT.TopNtupleAnalysis.getNNLOWeight(ev.MC_ttbar_afterFSR_pt, ev.MC_t_afterFSR_pt, 2)*ROOT.TopNtupleAnalysis.getNNLOWeight(ev.MC_ttbar_afterFSR_pt, ev.MC_t_afterFSR_pt, 1)
